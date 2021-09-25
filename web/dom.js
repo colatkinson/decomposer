5640ecbbbcfac06f8a52dba06430b5f6366cbffe
Colin Atkinson <colin.william.atkinson@gmail.com>
2021-09-25 07:09:38 -0400

    Refactor dom.js wasm calls, fix mem management

--- web/dom.js
+++ web/dom.js
@@ -14,23 +14,29 @@ function connectEvents() {
     };
 }
 
+function _decompose_flags(cstr) {
+    const res = Module.ccall("em_decompose_flags", "number", ["number"], [cstr])
+    if (res === 0) {
+        throw "Ran out of memory -- got null from em_decompose_flags";
+    }
+
+    try {
+        const outStr = UTF8ToString(res);
+        return outStr;
+    } finally {
+        Module.ccall("em_free", null, ["number"], [res]);
+    }
+}
+
 function processString(str) {
     const len = lengthBytesUTF8(str);
     const buf = Module._malloc(len + 1);
 
     try {
         stringToUTF8(str, buf, len + 1);
-
-        const res = Module.ccall("em_decompose_flags", "number", ["number"], [buf])
-        if (res === 0) {
-            throw "Ran out of memory -- got null from em_decompose_flags";
-        }
-
-        const outStr = UTF8ToString(res);
-
-        return outStr;
+        return _decompose_flags(buf);
     } finally {
-        Module.ccall("em_free", null, ["number"], [buf])
+        Module._free(buf);
     }
 }
 
