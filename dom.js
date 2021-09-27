9c65ae063a3aa8b9b5a3888165ddf4fc6acab3cc
colatkinson <colatkinson@users.noreply.github.com>
2021-09-27 10:30:58 +0000

    Deploying to gh-pages from @ colatkinson/decomposer@b62b457f044b8c5347d61161465ed25ab6d18109 🚀

---
+++ dom.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var Module = {};
+
+function connectEvents() {
+    document.getElementById("num_submit").disabled = false;
+
+    document.getElementById("num_form").onsubmit = function(e) {
+        e.preventDefault();
+
+        const outElem = document.getElementById("num_out");
+
+        try {
+            const res = processString(document.getElementById("num_input").value);
+            outElem.innerText = res;
+        } catch (err) {
+            outElem.innerText = "";
+        }
+    };
+}
+
+function _decompose_flags(cstr) {
+    const res = Module.ccall("em_decompose_flags", "number", ["number"], [cstr])
+    if (res === 0) {
+        throw "Ran out of memory -- got null from em_decompose_flags";
+    }
+
+    try {
+        const outStr = UTF8ToString(res);
+        return outStr;
+    } finally {
+        Module.ccall("em_free", null, ["number"], [res]);
+    }
+}
+
+function processString(str) {
+    const len = lengthBytesUTF8(str);
+    const buf = Module._malloc(len + 1);
+
+    try {
+        stringToUTF8(str, buf, len + 1);
+        return _decompose_flags(buf);
+    } finally {
+        Module._free(buf);
+    }
+}
+
+Module["onRuntimeInitialized"] = connectEvents;
